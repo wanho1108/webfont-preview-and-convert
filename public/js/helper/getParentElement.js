da3aabec145672446f16e4fc02f02d2407274722
wanho1108 <wanho1108@gmail.com>
2020-02-29 13:39:40 +0900

    탭 디자인 추가

---
+++ public/js/helper/getParentElement.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  function getParentElement(currentElement, conditionFunction) {
+    let element = currentElement;
+
+    while (conditionFunction(element) === false) {
+      element = element.parentElement;
+      if (element === null) break;
+    }
+
+    return element;
+  }
+
+  window.getParentElement = getParentElement;
+
+})();
--- public/js/ui.tabs.js
+++ public/js/ui.tabs.js
@@ -8,21 +8,37 @@
     $element.addEventListener('keyup', tabsKeyupHandler);
   });
 
+  function tabsBarAnimation($tabs, index) {
+    const $tabsButtonActive = $tabs.querySelectorAll('[role="tab"]')[index];
+    const $bar = $tabs.querySelector('.tabs__header-bar');
+    const { offsetLeft: elementOffsetLeft, clientWidth: elementWidth } = $tabsButtonActive;
+    const barWidth = $bar.clientWidth;
+    const taretLeft = elementOffsetLeft + (elementWidth - barWidth) / 2;
+
+    $bar.style.left = `${taretLeft}px`;
+  }
+
   function tabsClickHandler(e) {
     const $target = e.target;
 
-    if ($target.classList.contains('tabs__navigation-button')) {
-      const $sibglins = Array.prototype.filter.call($target.parentNode.childNodes, node => node.nodeName !== '#text');
-      const $panels = $target.parentNode.parentNode.querySelectorAll('[role="tabpanel"]');
+    if ($target.getAttribute('role') === 'tab') {
+      const $tabs = getParentElement($target, element => element.classList.contains('tabs'));
+      const $sibglins = $tabs.querySelectorAll('[role="tab"]');
+      const $panels = $tabs.querySelectorAll('[role="tabpanel"]');
+
       $sibglins.forEach(($element, index) => {
         if ($target === $element) {
           $element.setAttribute('aria-selected', true);
           $element.removeAttribute('tabindex');
+          $panels[index].setAttribute('aria-hidden', false);
           $panels[index].removeAttribute('hidden');
+          tabsBarAnimation($tabs, index);
           return;
         }
+
         $element.setAttribute('aria-selected', false);
         $element.setAttribute('tabindex', '-1');
+        $panels[index].setAttribute('aria-hidden', true);
         $panels[index].setAttribute('hidden', true);
       });
     }
@@ -31,7 +47,7 @@
   function tabsKeyupHandler(e) {
     const $target = e.target;
 
-    if ($target.classList.contains('tabs__navigation-button')) {
+    if ($target.getAttribute('role') === 'tab') {
       switch (e.keyCode) {
         case 37 : // 방향키 좌측
           const $previous = $target.previousElementSibling;
@@ -41,7 +57,7 @@
             $previous.click();
           }
           break;
-        case 39 : // 방향키 우측측
+        case 39 : // 방향키 우측
           const $next = $target.nextElementSibling;
 
           if ($next) {
